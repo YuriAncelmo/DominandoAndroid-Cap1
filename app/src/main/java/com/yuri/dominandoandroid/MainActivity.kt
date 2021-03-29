de9c4d1d0df12befb0d5043977c183108b8f2cd4
yuria <yuriancelmo@hotmail.com>
2021-03-29 10:33:39 -0300

    teste

--- app/src/main/java/com/yuri/dominandoandroid/MainActivity.kt
+++ app/src/main/java/com/yuri/dominandoandroid/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     //Em kotlin , object é Any
     //Unit é void
-    //Funções são públicas por padrão
+    //Funções são públicas por padrão 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
